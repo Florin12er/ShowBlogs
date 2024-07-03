9883e000f6c191e74ea5beacf34d33d3d141303f
florin <apetreiflorin236@gmail.com>
2024-07-03 09:15:18 +0200

    added jwt to the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(
+          "https://blogapi-production-fb2f.up.railway.app/user",
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
+    }
+
+    fetchData();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-blue-300'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      {data && data.length > 0 ? (
+        <ul>
+          {data.map((item) => (
+            <li key={uuidv4()}>
+              <div>
+                <p>Username: {item.username}</p>
+                <p>Email: {item.email}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p>No data available</p>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
