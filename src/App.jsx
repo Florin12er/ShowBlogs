e30f593ca8f7cd4afafbaee9da4fff343eca2b82
florin <apetreiflorin236@gmail.com>
2024-07-07 16:39:45 +0200

    test github auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import RequireAuth from "./components/PrivateRoute";
+import NavBar from "./components/NavBar";
 import ShowAllBlogs from "./routes/showAllBlogs";
 import ShowBlog from "./routes/showBlog";
 import ShowAllUsers from "./routes/showAllUsers";
@@ -9,59 +10,65 @@ import Register from "./routes/Register";
 import ResetPassword from "./routes/Reset";
 import ResetRequest from "./routes/ResetRequest";
 import Settings from "./routes/Setting";
-import GithubCallback from "./routes/GithubCallback"; // Ensure correct import
+import GithubCallback from "./routes/GithubCallback";
 
+import YourBlogs from "./routes/YourBlogs";
+import Update from "./routes/Update";
+import { ThemeProvider } from "./components/ThemeContext";
 function App() {
   return (
-    <Routes>
-      <Route path="/auth/github/callback/:token" element={<GithubCallback />} />
-      <Route
-        path="/"
-        element={
-          <RequireAuth>
-            <ShowAllBlogs />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/blog/:id"
-        element={
-          <RequireAuth>
-            <ShowBlog />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/user"
-        element={
-          <RequireAuth>
-            <ShowAllUsers />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/user/:id"
-        element={
-          <RequireAuth>
-            <ShowUser />
-          </RequireAuth>
-        }
-      />
-      <Route
-        path="/settings/:id"
-        element={
-          <RequireAuth>
-            <Settings />
-          </RequireAuth>
-        }
-      />
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/reset-request" element={<ResetRequest />} />
-      <Route path="/reset" element={<ResetPassword />} />
-    </Routes>
+    <>
+      <ThemeProvider>
+        <Routes>
+                    <Route path="/auth/github/callback/:token" element={<GithubCallback />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <ShowAllBlogs />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/blog/:id"
+            element={
+              <RequireAuth>
+                <ShowBlog />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/user"
+            element={
+              <RequireAuth>
+                <ShowAllUsers />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/user/:id"
+            element={
+              <RequireAuth>
+                <ShowUser />
+              </RequireAuth>
+            }
+          />
+          <Route
+            path="/settings/:id"
+            element={
+              <RequireAuth>
+                <Settings />
+              </RequireAuth>
+            }
+          />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset-request" element={<ResetRequest />} />
+          <Route path="/reset" element={<ResetPassword />} />
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
 export default App;
-
