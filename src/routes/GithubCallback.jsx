9f1f129b0f7151c7221282ef2bdbf30c611383f0
florin <apetreiflorin236@gmail.com>
2024-07-07 16:08:22 +0200

    updated github auth

--- src/routes/GithubCallback.jsx
+++ src/routes/GithubCallback.jsx
@@ -6,17 +6,6 @@ function GitHubCallback() {
     const navigate = useNavigate();
     const location = useLocation();
 
-    useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const token = queryParams.get("token");
-
-        if (token) {
-            console.log("Received token:", token);
-            localStorage.setItem("token", token);
-        }
-
-        navigate("/"); // Replace with your desired route after successful GitHub authentication
-    }, [location, navigate]);
 
     return (
         <h1>Hello</h1>
